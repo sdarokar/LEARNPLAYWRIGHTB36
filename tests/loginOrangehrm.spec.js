718ccc4f00753c6038b83a08f4b1da950155c09a
sdarokar <satish.darokar@gmail.com>
2026-03-25 21:37:16 +0530

    added script to env variable

--- tests/loginOrangehrm.spec.js
+++ tests/loginOrangehrm.spec.js
@@ -2,11 +2,18 @@ import { test, expect } from '@playwright/test';
 import logindata from "../testdata/OHRMlogin.json"
 
 test('Login to Orange HRM', async ({ page }) => {
-
-    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    // Using testdata
+    /*await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     await page.locator('//input[@name="username"]').fill(logindata.username)
     await page.locator('//input[@name="password"]').fill(logindata.password)
     await page.locator('//button[@type="submit"]').click()
-    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')*/
+
+    //Using .env
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.locator('//input[@name="username"]').fill(process.env.APP_USERNAME)
+    await page.locator('//input[@name="password"]').fill(process.env.APP_USERNAME)
+    await page.locator('//button[@type="submit"]').click()
+    //await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
 
 })
