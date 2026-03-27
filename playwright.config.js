c43d48202c051177ea2ce69010d793f03ea0a525
sdarokar <satish.darokar@gmail.com>
2026-03-27 18:29:27 +0530

    Configure

--- playwright.config.js
+++ playwright.config.js
@@ -30,10 +30,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: 'html',
+  reporter: [['html'],['List'],['json', {  outputFile: 'results.json' }]],
   //reporter: 'List',
-  //reporter: 'dot',
-  reporter:[['json', {  outputFile: 'results.json' }]],
+  //reporter: 'dot',npx playwright test --project=chromium --headed
+  //reporter:[['json', {  outputFile: 'results.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto(''npx playwright test)`. */
